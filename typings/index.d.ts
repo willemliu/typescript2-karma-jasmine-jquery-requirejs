df1d41851e371f731652512745e209ad8b6a4b44
Willem Liu <willem_liu@hotmail.com>
2016-09-26 23:18:41 +0200

    Removed unused dependencies in typings.json

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,3 @@
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/jsdom/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="modules/requirejs/index.d.ts" />
